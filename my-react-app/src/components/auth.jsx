d51f0e78e6a07ea30a1074e0939ba450035ef062
Surabhi-M-R <surabhimr27@gmail.com>
2025-06-03 22:37:41 +0530

    react-firebase

---
+++ my-react-app/src/components/auth.jsx
@@ -0,0 +1,9 @@
+export const Auth = () => {
+    return (
+        <div>
+            <input placeholder="Email..."/> 
+            <input placeholder="Password..."/> 
+            <button>Sign In</button>
+        </div>
+    );
+};
---
+++ my-react-app/src/config/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKDZThw5BbeH5A9K3AUBHvJwTqCmqn-F8",
+  authDomain: "fir-course-31890.firebaseapp.com",
+  projectId: "fir-course-31890",
+  storageBucket: "fir-course-31890.firebasestorage.app",
+  messagingSenderId: "1009104734797",
+  appId: "1:1009104734797:web:b7ea5e655fe412b1ab6a04",
+  measurementId: "G-DEV4GQJEVM"
+};
+
+
+const app = initializeApp(firebaseConfig);
+ export const auth = getAuth(app)
